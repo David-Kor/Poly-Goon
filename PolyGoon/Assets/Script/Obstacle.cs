f1a71faa46f96359a1158a2f62229eda10c84e06
David-Kor <36980670+David-Kor@users.noreply.github.com>
2018-11-28 17:19:29 +0900

    [다윗] - 베이스 도형 이미지 추가 - 플레이어 움직임 적용 - 물리 및 충돌 감지 적용 - 충돌 시 플레이어의 Death 이벤트 적용(현재 동작은 디버그 출력 뿐)

---
+++ PolyGoon/Assets/Script/Obstacle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public float speed = 2.0f;
+
+    void Update()
+    {
+        transform.localScale -= Vector3.one * speed * Time.deltaTime;
+        if (transform.localScale.x < 0) { Destroy(gameObject); }
+    }
+}
---
+++ PolyGoon/Assets/Script/Screen.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Screen : MonoBehaviour
+{
+    public GameObject screenObject;    //화면에 출력될 이미지 오브젝트
+
+    void Start()
+    {
+        Instantiate(screenObject, Vector3.zero, Quaternion.Euler(Vector3.zero), transform);
+    }
+}
